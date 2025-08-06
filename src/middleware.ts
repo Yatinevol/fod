19c27e618723862bf0ebb22cbfae1babf5b5c96e
Yatin Singh <140312835+Yatinevol@users.noreply.github.com>
2025-08-06 22:58:00 +0530

    Implement POST endpoint for goal creation

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
-
+// this protects only client side routing and not the api routes so its better to use auth to verify session and token in api routes.
 
 export async function middleware(request:NextRequest) {
     const token = await getToken({req: request, secret: process.env.AUTH_SECRET})
