6d377b3430ee1e17d53348dc69c7e999db7e110b
Yatin Singh <140312835+Yatinevol@users.noreply.github.com>
2025-08-06 18:49:06 +0530

    add middleware on protected routes

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+
+export async function middleware(request:NextRequest) {
+    const token = await getToken({req: request, secret: process.env.AUTH_SECRET})
+    const url = request.nextUrl
+
+    if(!token && (url.pathname.startsWith('/goal')
+                || url.pathname.startsWith('/dashboard')
+                || url.pathname.startsWith('/streak')
+    )){
+        return NextResponse.redirect(new URL('/sign-in', request.url))
+    }
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        '/dashboard/:path*', '/goal/:path*', '/streak/:path*'
+    ]
+}
