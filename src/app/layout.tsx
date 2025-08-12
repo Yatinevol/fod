f2b1e28ca04a4ae0ab5c7a44aaf256b44de54597
Yatin_Singh <yatinsingh331@gmail.com>
2025-08-12 22:40:18 +0530

    sign page ui improvements, tried glassmorphism

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./styles/globals.css";
+import "./styles/signup.module.css"
 import { Toaster } from "sonner";
 
 const geistSans = Geist({
