68e71cdb454e2872e2c5d5eb4c1e9a73a68a2550
Yatin Singh <140312835+Yatinevol@users.noreply.github.com>
2025-08-17 22:21:16 +0530

    working on goal page

--- src/app/goal/page.tsx
+++ src/app/goal/page.tsx
@@ -3,13 +3,24 @@ import DateTime from '@/components/DateTime';
 import React, { useEffect, useState } from 'react'
 
 const Goal = () => {
-  
+  const [category,setCategory] = useState("")
   return (
     <div>
       <header className='flex justify-between items-center'>
         <h1 className="text-2xl font-bold">Tasks</h1>
+        <div className='space-x-6'>
+          <input type="text" placeholder='Search...' value={category} onChange={(e)=>setCategory(e.target.value)} className='bg-white p-3 w-96 rounded-xl' /> 
+
+          <button className='bg-blue-600 text-white px-3 py-2'>
+            Add Category
+          </button>
+        </div>
         <DateTime/>
       </header>
+
+      <main>
+        
+      </main>
     </div>
   );
 }
