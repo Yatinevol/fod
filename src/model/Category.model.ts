877623d5f9b2907fdbb0df2c853d3ad790273561
Yatin_Singh <yatinsingh331@gmail.com>
2025-08-22 23:15:37 +0530

    added category post route and category model

---
+++ src/model/Category.model.ts
@@ -0,0 +1,31 @@
+import z from "zod";
+import mongoose,{ Schema} from "mongoose";
+import { User } from "./User.model";
+import { Types } from "mongoose";
+
+
+export interface CategoryI{
+    name:string;
+    userId: Types.ObjectId | User
+    createdAt?: Date;
+}
+
+const CategorySchema = new Schema<CategoryI>({
+    name: { 
+        type: String, 
+        required: true,
+         unique: true
+         }
+    ,
+    userId:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+    
+
+},{timestamps:true})
+
+const CategoryModal = (mongoose.models.Category as mongoose.Model<CategoryI>) || mongoose.model<CategoryI>("Category",CategorySchema)
+
+export default CategoryModal
