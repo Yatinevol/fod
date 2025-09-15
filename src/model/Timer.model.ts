4aa000ab0803305a675fc0423af650fb29fbe54b
Yatin Singh <140312835+Yatinevol@users.noreply.github.com>
2025-09-16 01:34:44 +0530

    add Timer model with TaskEntry schema and user association

--- src/model/Timer.model.ts
+++ src/model/Timer.model.ts
@@ -1,2 +1,51 @@
-import mongoose from "mongoose";
-import z from "zod";
+import mongoose,{ Schema, Types} from "mongoose";
+import { GoalI } from "./Goal.model";
+import { User } from "./User.model";
+
+export interface TaskEntry {
+    taskId: Types.ObjectId | GoalI;
+    title: string;
+    totalFocusMinutes: number;
+}
+const TaskEntrySchema = new Schema({
+    goalId: { type: Schema.Types.ObjectId, ref: 'Goal', required: true },
+    title: { type: String, required: true },
+    totalFocusMinutes: { type: Number, required: true }
+});
+
+export interface TimerI{
+    userId: Types.ObjectId | User ;
+    date : string;
+    totalFocusMinutes: number;
+    targetMinutes: number;
+    tasks: TaskEntry[]
+}
+
+const timerSchema  = new Schema<TimerI>({
+    userId: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'User', required: true 
+    },
+
+    date: { 
+        type: String, 
+        required: true 
+    },
+    totalFocusMinutes: { 
+        type: Number, 
+        required: true 
+    },
+    targetMinutes: { 
+        type: Number, 
+        required: true 
+    },
+    tasks: { 
+        type: [TaskEntrySchema], 
+        default: [] 
+    }
+},{timestamps:true})
+
+const TimerModel = (mongoose.models.Timer as mongoose.Model<TimerI>) ||
+    (mongoose.model("Timer",timerSchema))
+
+export default TimerModel
